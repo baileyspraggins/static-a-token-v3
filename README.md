c7f540637ab041c92497e041a438ee3846f41db5
baileyspraggins <baileyspraggins@me.com>
2023-02-08 18:50:22 -0600

    Deployment script and instructions

--- README.md
+++ README.md
@@ -17,10 +17,47 @@ forge install
 // Step 2. Compile all solidity contracts
 forge build
 
-// Step 3. Run all unit/fork tests
+// Step 3. Add your Etherscan api key, RPC endpoints within the .env file
+
+
+// Step 4. Run all unit/fork tests
 forge test
 ```
 
 ## Deploying Static aToken token wrappers
 
-Coming soon...
+1. Within the .env set up the details for your token wrapper deployment
+
+```
+# Values for token deployments
+aTokenName="<Wrapped Token Name>"
+aTokenSymbol="<Wrapped Token Symbol>"
+poolAddress=<Aave Lending Pool Address>
+adminAddress=<Proxy Admin Address>
+aTokenAddress=<aToken Implementation Address>
+```
+
+2. Run the following command to deploy your token wrapper
+
+```
+forge script script/AaveV3Wrapper.s.sol:AaveV3Wrapper --rpc-url $<.env Network RPC Variable> --broadcast --verify -vvvv
+```
+
+Below is an example of a deployment for aDAI on the Goerli Testnet
+
+.env values:
+
+```
+# Values for token deployments
+aTokenName="Wrapped aEthDAI"
+aTokenSymbol="waEthDAI"
+poolAddress=0x7b5C526B7F8dfdff278b4a3e045083FBA4028790
+adminAddress=0x263c0a1ff85604f0ee3f4160cAa445d0bad28dF7
+aTokenAddress=0xADD98B0342e4094Ec32f3b67Ccfd3242C876ff7a
+```
+
+Deployment command:
+
+```
+forge script script/AaveV3Wrapper.s.sol:AaveV3Wrapper --rpc-url $RPC_GOERLI --broadcast --verify
+```
--- script/AaveV3Wrapper.s.sol
+++ script/AaveV3Wrapper.s.sol
@@ -1,16 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
 import "forge-std/Script.sol";
 import "../src/StaticATokenLM.sol";
 
+import {TransparentProxyFactory} from 'solidity-utils/contracts/transparent-proxy/TransparentProxyFactory.sol';
+
+
 // Set up of Wrapper Deploy Script
 contract AaveV3Wrapper is Script {
+    string private _aTokenName;
+    string private _aTokenSymbol;
+    StaticATokenLM private _staticATokenLM;
+    address private _aToken;
+    address private _proxyAdmin;
+    IPool private _pool;
+
     function run() external {
+        // Transparent Proxy Factory will allow us to create a new proxy wrapper
+        // that points to an Aave aToklen implementation 
+
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-
         vm.startBroadcast(deployerPrivateKey);
+        
+        TransparentProxyFactory proxyFactory = new TransparentProxyFactory();
+        StaticATokenLM staticATokenLMImpl = new StaticATokenLM();
 
-        StaticATokenLM aaveWrapper = new StaticATokenLM()
+        loadEnvVariables();
+
+        _staticATokenLM = StaticATokenLM(
+            proxyFactory.create(
+                address(staticATokenLMImpl),
+                _proxyAdmin,
+                abi.encodeWithSelector(
+                    StaticATokenLM.initialize.selector,
+                    _pool,
+                    _aToken,
+                    _aTokenName,
+                    _aTokenSymbol
+                )
+            )
+        );
+
+        vm.stopBroadcast();
     }
+
+    function loadEnvVariables() internal {
+        _aTokenName = vm.envString("aTokenName");
+        _aTokenSymbol = vm.envString("aTokenSymbol");
+        _aToken = vm.envAddress("aTokenAddress");
+        _proxyAdmin = vm.envAddress("adminAddress");
+        _pool = IPool(vm.envAddress("poolAddress"));
+    }
+
 }
